65fde299cc381c5b8a8ed30df639809f5da8e8c6
ParkSeYun98 <yun12343@naver.com>
2023-10-17 16:51:15 +0900

    Feat: CustomResponse Setting

---
+++ Back/src/main/java/com/lingotown/global/response/CommonResponse.java
@@ -0,0 +1,13 @@
+package com.lingotown.global.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class CommonResponse {
+
+    private String code;
+    private String message;
+}
