dc1907cf8a135a67930e19d25e839c42af2a9f83
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-10-17 18:54:59 +0900

    [feat/BE] create entity, enum

---
+++ Back/src/main/java/com/lingotown/domain/membernpc/entity/MemberNPC.java
@@ -0,0 +1,42 @@
+package com.lingotown.domain.membernpc.entity;
+
+import com.lingotown.domain.member.entity.Member;
+import com.lingotown.domain.npc.entity.NPC;
+import com.lingotown.domain.talk.entity.Talk;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_npc")
+public class MemberNPC {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_npc_id")
+    private Long id;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int intimacy;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "npc_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private NPC npc;
+
+    @OneToMany(mappedBy = "talk",cascade = CascadeType.ALL)
+    List<Talk> talkList = new ArrayList<>();
+}
