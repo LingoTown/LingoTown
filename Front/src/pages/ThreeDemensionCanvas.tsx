7ef30ff978084b6c0bf0814be428670b4b414bfb
신산하 <tksgk2598@naver.com>
2023-10-26 16:13:50 +0900

    Feat/FE: Bar 배경 추가 및 npc 위치

--- Front/src/pages/ThreeDemensionCanvas.tsx
+++ Front/src/pages/ThreeDemensionCanvas.tsx
@@ -2,10 +2,11 @@ import {useState} from 'react';
 import { Canvas } from "@react-three/fiber";
 import { BasketBallCourtTheme } from "../component/place/BasketBallCourtTheme";
 import { ParkTheme } from '../component/place/ParkTheme';
+import {BarTheme} from '../component/place/BarTheme';
 
 export const ThreeDemensionCanvas = () => {
 
-  const [place, setPlace] = useState<number>(1);//유저가 선택한 맵이 recoil에 저장되고 불러와야 함
+  const [place, setPlace] = useState<number>(3);//유저가 선택한 맵이 recoil에 저장되고 불러와야 함
 
   return(
     <>
@@ -14,7 +15,9 @@ export const ThreeDemensionCanvas = () => {
       }}> 
         {
           place===1?<BasketBallCourtTheme/>:(
-            place===2?<ParkTheme/>:null
+            place===2?<ParkTheme/>:(
+              place===3?<BarTheme/>:null
+            )
           )
         }
       </Canvas>
