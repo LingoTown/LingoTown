966b5992317534c55ad881e33386f1cf7292c4cb
ParkSeYun98 <yun12343@naver.com>
2023-10-26 15:33:01 +0900

    Feat/BE: WebClient Config 추가

--- Back/src/main/java/com/lingotown/global/config/WebClientConfig.java
+++ Back/src/main/java/com/lingotown/global/config/WebClientConfig.java
@@ -1,12 +1,15 @@
 package com.lingotown.global.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import java.time.Duration;
+import reactor.netty.resources.ConnectionProvider;
 
 import reactor.netty.http.client.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
