9b7c2a16ee8c8439ad0cd6b7b7b209216ae8e5a3
ParkSeYun98 <yun12343@naver.com>
2023-10-30 13:42:07 +0900

    Feat/BE: TTS Logic

--- Back/src/main/java/com/lingotown/global/tts/TTSService.java
+++ Back/src/main/java/com/lingotown/global/tts/TTSService.java
@@ -2,17 +2,26 @@ package com.lingotown.global.tts;
 
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
+import com.lingotown.global.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class TTSService {
 
-    public void UseTTS() throws Exception{
+    private S3Service s3Service;
+
+    public String UseTTS() throws Exception{
         // textToSpeechClient 초기화
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
             // Text 입력
@@ -24,6 +33,7 @@ public class TTSService {
                     VoiceSelectionParams.newBuilder()
                             .setLanguageCode("en-US")
                             .setName("en-US-Wavenet-F")
+                            .setSsmlGender(SsmlVoiceGender.FEMALE)
                             .build();
 
             // 리턴 받을 오디오 타입
@@ -39,10 +49,17 @@ public class TTSService {
             ByteString audioContents = response.getAudioContent();
 
             // Write the response to the output file.
-            try (OutputStream out = new FileOutputStream("output.mp3")) {
-                out.write(audioContents.toByteArray());
-                System.out.println("Audio content written to file \"output.mp3\"");
-            }
+//            try (OutputStream out = new FileOutputStream("output.mp3")) {
+//                out.write(audioContents.toByteArray());
+//                System.out.println("Audio content written to file \"output.mp3\"");
+//            }
+
+            // Convert ByteString to MultipartFile
+            MultipartFile audioFile = new MockMultipartFile("file", "output.mp3", "audio/mpeg", audioContents.toByteArray());
+
+            String fileUrl = s3Service.uploadFile(audioFile);
+
+            return fileUrl;
         }
     }
 }
