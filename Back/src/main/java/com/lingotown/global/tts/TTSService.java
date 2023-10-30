c89c1ac836da94889a67ecc19045649f974b04ba
ParkSeYun98 <yun12343@naver.com>
2023-10-30 15:04:35 +0900

    Fix/BE: TTS MultipartFile 리턴하도록 변경

--- Back/src/main/java/com/lingotown/global/tts/TTSService.java
+++ Back/src/main/java/com/lingotown/global/tts/TTSService.java
@@ -2,8 +2,10 @@ package com.lingotown.global.tts;
 
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
+import com.lingotown.global.aspect.ExecuteTime.TrackExecutionTime;
 import com.lingotown.global.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,18 +16,23 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class TTSService {
 
-    private S3Service s3Service;
+    private final S3Service s3Service;
 
-    public String UseTTS() throws Exception{
+    @TrackExecutionTime
+    public MultipartFile UseTTS(String prompt) throws Exception{
+
+        log.info(prompt);
+
         // textToSpeechClient 초기화
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
             // Text 입력
-            SynthesisInput input = SynthesisInput.newBuilder().setText("Hello, World! I'm SeYun").build();
+            SynthesisInput input = SynthesisInput.newBuilder().setText(prompt).build();
 
             // 언어 : en-US
             // 모델 : neutral
@@ -48,18 +55,15 @@ public class TTSService {
             // Get the audio contents from the response
             ByteString audioContents = response.getAudioContent();
 
-            // Write the response to the output file.
-//            try (OutputStream out = new FileOutputStream("output.mp3")) {
-//                out.write(audioContents.toByteArray());
-//                System.out.println("Audio content written to file \"output.mp3\"");
-//            }
+// Convert ByteString to MultipartFile without saving to a file
+            MultipartFile audioFile = new MockMultipartFile(
+                    "file",
+                    "output.mp3",
+                    "audio/mpeg",
+                    audioContents.toByteArray()
+            );
 
-            // Convert ByteString to MultipartFile
-            MultipartFile audioFile = new MockMultipartFile("file", "output.mp3", "audio/mpeg", audioContents.toByteArray());
-
-            String fileUrl = s3Service.uploadFile(audioFile);
-
-            return fileUrl;
+            return audioFile;
         }
     }
 }
