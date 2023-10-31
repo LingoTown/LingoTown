7fe410b952943b978ddb4a1039711bb5c8cd4b28
leverest96 <myjade0416@gmail.com>
2023-10-31 17:25:05 +0900

    Feat/FE: NPC 캐릭터 소개

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -5,8 +5,10 @@ import GoogleCallback from "./component/login/GoogleCallbackComp";
 import MainPage from './pages/MainPage';
 import NotFound from './pages/NotFoundPage';
 import { CanvasPage } from "./pages/CanvasPage";
+import { IntroducePage } from "./pages/IntroducePage";
 import { RestaurantComp } from "./component/theme/RestaurantComp";
 import { PolyginiaComp } from "./component/theme/PolyginiaComp";
+import { IntroduceComp } from "./component/introduce/IntroduceComp";
 
 
 const App = () => {
@@ -18,7 +20,8 @@ const App = () => {
         <Route path="/google/callback" element={ <GoogleCallback/> }/>
         <Route path="/main" element={ <MainPage/> }/>
         <Route path="/restaurant" element={ <CanvasPage theme={ <RestaurantComp/> }/> }/>
-        <Route path="/polyginia" element={ <CanvasPage theme={ <PolyginiaComp/> }/> }/>
+        <Route path="/polyginia" element={<CanvasPage theme={<PolyginiaComp />} />} />
+        <Route path="/introduce" element={ <IntroducePage theme={ <IntroduceComp/> }/> }/>
         <Route path="/*" element={ <NotFound/> }/>
       </Routes>
     </BrowserRouter>
