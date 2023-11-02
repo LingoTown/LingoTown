85a4dea0e038afede7f2a7eef51bf61b9c97e009
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-11-03 01:34:13 +0900

    Feat/BE: create quiz table 퀴즈 테이블 생성 및 테마와 연결

--- Back/src/main/java/com/lingotown/domain/world/entity/Quiz.java
+++ Back/src/main/java/com/lingotown/domain/world/entity/Quiz.java
@@ -1,10 +1,16 @@
 package com.lingotown.domain.world.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.checkerframework.checker.units.qual.C;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "quiz")
 public class Quiz {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
