bd68fc37ced46c567128ac51ef47a73913b5db86
ParkSeYun98 <yun12343@naver.com>
2023-11-03 15:52:18 +0900

    Feat/FE: House Map Asset S3로 이동

--- Front/public/map/house/House.jsx
+++ Front/public/map/house/House.jsx
@@ -11,7 +11,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function House(props) {
-  const { nodes, materials } = useGLTF('./map/house/scene.gltf')
+  const { nodes, materials } = useGLTF('https://b305finalproject.s3.ap-northeast-2.amazonaws.com/Map/House/scene.gltf')
   return (
     <group {...props} dispose={null}>
       <group scale={0.0015}>
