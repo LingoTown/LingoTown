0cc8855041ec10a95d352c6ac4e8bd3e7734a33e
ParkSeYun98 <yun12343@naver.com>
2023-11-03 14:28:51 +0900

    Fix/FE: Wall Opacity Setting

--- Front/src/component/util/block/Wall.tsx
+++ Front/src/component/util/block/Wall.tsx
@@ -5,6 +5,7 @@ import { wallType } from '../../../type/WallType';
 export const Wall: React.FC<wallType> = ({
   size,
   position,
+  rotation,
   wallKey,
   name,
   mass,
@@ -12,6 +13,7 @@ export const Wall: React.FC<wallType> = ({
   const [ref]:any = useBox(() => ({
     args: size,
     position : position,
+    rotation: rotation,
     mass,
     friction: 1,    // Adjust the value as needed
     restitution: 0,  // Set to 0 to avoid bouncing
@@ -20,7 +22,7 @@ export const Wall: React.FC<wallType> = ({
   return (
     <mesh ref={ref} name={name} key={wallKey}>
       <boxGeometry args={size}/>
-      {/* <meshStandardMaterial transparent opacity={0}/> */}
+      <meshStandardMaterial transparent opacity={0}/>
     </mesh>
   );
 };
