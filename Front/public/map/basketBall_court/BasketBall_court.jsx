1f3c9144fc3680fd2a00dfab5beb5bb21dbf7cf4
ParkSeYun98 <yun12343@naver.com>
2023-11-03 15:49:15 +0900

    Feat/FE: BasketBall_Court Map Asset S3로 이동

--- Front/public/map/basketBall_court/BasketBall_court.jsx
+++ Front/public/map/basketBall_court/BasketBall_court.jsx
@@ -11,7 +11,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function BasketBall_court(props) {
-  const { nodes, materials } = useGLTF('./map/basketBall_court/scene.gltf')
+  const { nodes, materials } = useGLTF('https://b305finalproject.s3.ap-northeast-2.amazonaws.com/Map/BasketBall_Court/scene.gltf')
   return (
     <group {...props} dispose={null}>
       <group scale={0.01}>
