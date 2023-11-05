936b1cfe0845e854234bacf56b7ac4e99f4d11e1
박세윤 <yun12343@naver.com>
2023-11-05 23:49:40 +0900

    Fix/FE: Console.log 삭제

--- Front/src/component/theme/util/KeyboardUtil.ts
+++ Front/src/component/theme/util/KeyboardUtil.ts
@@ -1,50 +1,72 @@
-export const HandleKeyDown = (SetAction: (actionName: string, activeAction: any, actions: any, playerRef: any) => void, keysPressed: any, activeAction: any, actions: any, isMove: React.RefObject<boolean>, playerRef: any) => {
+export const HandleKeyDown = (
+  SetAction: (
+    actionName: string,
+    activeAction: any,
+    actions: any,
+    playerRef: any
+  ) => void,
+  keysPressed: any,
+  activeAction: any,
+  actions: any,
+  isMove: React.RefObject<boolean>,
+  playerRef: any
+) => {
   return (event: KeyboardEvent): void => {
     if (isMove.current) {
-      if (['ArrowUp', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
-        keysPressed.current[event.key as 'ArrowUp' | 'ArrowLeft' | 'ArrowRight'] = true;
-        SetAction('Run', activeAction, actions, playerRef);
+      if (["ArrowUp", "ArrowLeft", "ArrowRight"].includes(event.key)) {
+        keysPressed.current[
+          event.key as "ArrowUp" | "ArrowLeft" | "ArrowRight"
+        ] = true;
+        SetAction("Run", activeAction, actions, playerRef);
       }
-      if (['ArrowDown'].includes(event.key)) {
-        keysPressed.current[event.key as 'ArrowDown'] = true;
-        SetAction('Walk', activeAction, actions, playerRef);
+      if (["ArrowDown"].includes(event.key)) {
+        keysPressed.current[event.key as "ArrowDown"] = true;
+        SetAction("Walk", activeAction, actions, playerRef);
       }
 
       /* 사용자가 J 키를 눌렀을 때 */
-      if(event.key === 'j' || event.key === 'J') {
-
+      if (event.key === "j" || event.key === "J") {
         // State Setting
-        keysPressed.current['Victory'] = true;
-        
+        keysPressed.current["Victory"] = true;
+
         // Jump Trigger
-        SetAction('Victory', activeAction, actions, playerRef);
+        SetAction("Victory", activeAction, actions, playerRef);
       }
-
-
     }
   };
 };
 
-
-export const HandleKeyUp = (SetAction: (actionName: string, activeAction: any, actions: any, playerRef: any) => void, keysPressed: any, activeAction: any, actions: any, isMove: React.RefObject<boolean>, playerRef: any) => {
+export const HandleKeyUp = (
+  SetAction: (
+    actionName: string,
+    activeAction: any,
+    actions: any,
+    playerRef: any
+  ) => void,
+  keysPressed: any,
+  activeAction: any,
+  actions: any,
+  isMove: React.RefObject<boolean>,
+  playerRef: any
+) => {
   return (event: KeyboardEvent): void => {
     if (isMove.current) {
-      if (['ArrowUp', 'ArrowLeft', 'ArrowRight', 'ArrowDown'].includes(event.key)) {
-        keysPressed.current[event.key as 'ArrowUp' | 'ArrowLeft' | 'ArrowRight' | 'ArrowDown'] = false;
+      if (
+        ["ArrowUp", "ArrowLeft", "ArrowRight", "ArrowDown"].includes(event.key)
+      ) {
+        keysPressed.current[
+          event.key as "ArrowUp" | "ArrowLeft" | "ArrowRight" | "ArrowDown"
+        ] = false;
         if (Object.values(keysPressed.current).every(key => !key)) {
-          SetAction('Idle', activeAction, actions, playerRef);
+          SetAction("Idle", activeAction, actions, playerRef);
         }
       }
 
       /* 사용자가 J 키를 해제 했을 때 */
-      if(event.key === 'j' || event.key === 'J') {
-            
+      if (event.key === "j" || event.key === "J") {
         // State Setting
-        keysPressed.current['Victory'] = false;
+        keysPressed.current["Victory"] = false;
       }
-
     }
   };
 };
-
-
