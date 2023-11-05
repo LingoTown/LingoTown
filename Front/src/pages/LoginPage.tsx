ff6bead411e4974b353e9bded8ddbab22084826f
신산하 <tksgk2598@naver.com>
2023-11-05 22:35:54 +0900

    Fix/FE: 커서 이미지 S3 이동 후 경로 수정

--- Front/src/pages/LoginPage.tsx
+++ Front/src/pages/LoginPage.tsx
@@ -24,15 +24,15 @@ const LoginPage = () => {
       </div>
       <div className="z-10 mt-7">
         <img 
-          style={{ cursor: `url('/navigation_hover_small.png'), auto` }}
+          style={{ cursor: `url('https://b305finalproject.s3.ap-northeast-2.amazonaws.com/MousePointer/navigation_hover_small.png'), auto` }}
           className="h-10 rounded" 
           src='https://fitsta-bucket.s3.ap-northeast-2.amazonaws.com/kakaobtn.png' onClick={() => { kakaoLogin() }}/>
         <img 
-          style={{ cursor: `url('/navigation_hover_small.png'), auto` }}
+          style={{ cursor: `url('https://b305finalproject.s3.ap-northeast-2.amazonaws.com/MousePointer/navigation_hover_small.png'), auto` }}
           className="h-10 rounded mt-2" 
           src='https://fitsta-bucket.s3.ap-northeast-2.amazonaws.com/googlebtn.png' onClick={() => { googleLogin() }}/>
         <img 
-          style={{ cursor: `url('/navigation_hover_small.png'), auto` }}
+          style={{ cursor: `url('https://b305finalproject.s3.ap-northeast-2.amazonaws.com/MousePointer/navigation_hover_small.png'), auto` }}
           className="h-10 rounded mt-2 bg-[#ddd]" 
           src='https://fitsta-bucket.s3.ap-northeast-2.amazonaws.com/exp.png' onClick={() => { navigate("/restaurant") }}/>
       </div>
