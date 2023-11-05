5aec51cbcd070203d2a556d7521ba4c9220bec7d
신산하 <tksgk2598@naver.com>
2023-11-05 22:21:28 +0900

    Feat/FE: 마우스 커스텀 전체 적용

--- Front/src/component/util/confirm/CustomConfirm.tsx
+++ Front/src/component/util/confirm/CustomConfirm.tsx
@@ -24,16 +24,16 @@ const CustomConfirm = ({ title, message, onClickOK, onClickCancel }: ConfirmType
   }, [onClickCancel]);
 
   return (
-    <div className="z-50">
+    <div className="z-50" style={{ cursor: `url('/navigation_small.png'), auto` }}>
       <div className="fixed inset-0 bg-black bg-opacity-50" onClickCapture={(e) => e.stopPropagation()}></div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 p-6 bg-white rounded-lg shadow-xl">
         <h2 className="text-lg font-bold mb-4 border-b pb-2 font-['passero-one']">{title}</h2>
         <div className="mb-5 text-gray-600 font-bold text-lg font-['passero-one']">{message}</div>
         <div className="mt-5 flex justify-end space-x-2">
-          <button ref={okButtonRef} onClick={onClickOK} className="py-2 px-4 bg-blue-500 text-white rounded focus:ring focus:ring-blue-400 focus:ring-opacity-50">
+          <button ref={okButtonRef} onClick={onClickOK} style={{ cursor: `url('/navigation_hover_small.png'), auto` }} className="py-2 px-4 bg-blue-500 text-white rounded focus:ring focus:ring-blue-400 focus:ring-opacity-50">
             OK
           </button>
-          <button ref={cancelButtonRef} onClick={onClickCancel} className="py-2 px-4 bg-gray-200 text-gray-700 rounded focus:ring focus:ring-gray-400 focus:ring-opacity-50" autoFocus>
+          <button ref={cancelButtonRef} onClick={onClickCancel} style={{ cursor: `url('/navigation_hover_small.png'), auto` }} className="py-2 px-4 bg-gray-200 text-gray-700 rounded focus:ring focus:ring-gray-400 focus:ring-opacity-50" autoFocus>
             Cancel
           </button>
         </div>
--- Front/src/component/util/prompt/CustomPrompt.tsx
+++ Front/src/component/util/prompt/CustomPrompt.tsx
@@ -23,7 +23,7 @@ const CustomPrompt = ({ title, message, _default, onClickOK, onClickCancel }: Pr
   };
 
   return (
-    <div className="z-50">
+    <div className="z-50" style={{ cursor: `url('/navigation_small.png'), auto` }}>
       <div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity" onClickCapture={(e) => e.stopPropagation()}></div>
       <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 p-6 bg-white rounded-lg shadow-xl border border-gray-200" onSubmit={handleSubmit}>
         <h2 className="text-lg font-semibold mb-4 border-b pb-2 font-['passero-one']">{title}</h2>
@@ -36,10 +36,10 @@ const CustomPrompt = ({ title, message, _default, onClickOK, onClickCancel }: Pr
           autoFocus
         />
         <div className="mt-6 flex justify-end space-x-2">
-          <button className="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:ring-opacity-50 transition-colors" type="submit">
+          <button style={{ cursor: `url('/navigation_hover_small.png'), auto` }} className="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:ring-opacity-50 transition-colors" type="submit">
             OK
           </button>
-          <button onClick={onClickCancel} className="py-2 px-4 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 focus:ring focus:ring-gray-300 focus:ring-opacity-50 transition-colors" type="reset">
+          <button style={{ cursor: `url('/navigation_hover_small.png'), auto` }} onClick={onClickCancel} className="py-2 px-4 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 focus:ring focus:ring-gray-300 focus:ring-opacity-50 transition-colors" type="reset">
             Cancel
           </button>
         </div>
