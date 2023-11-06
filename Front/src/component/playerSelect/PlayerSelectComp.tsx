f59b58777f4550c82b3646f8b667cdb700dc21e4
ParkSeYun98 <yun12343@naver.com>
2023-11-06 22:37:36 +0900

    Feat/FE: 대표 캐릭터 수정 기능 진행중

--- Front/src/component/playerSelect/PlayerSelectComp.tsx
+++ Front/src/component/playerSelect/PlayerSelectComp.tsx
@@ -3,6 +3,10 @@ import { useGLTF, Environment, Text } from "@react-three/drei";
 import * as THREE from 'three';
 import { userAtom } from "../../atom/UserAtom";
 import { useRecoilState } from 'recoil';
+import { updateCharacter } from "../../api/User"
+import { UpdateSelectedCharacter } from "../../type/UserType";
+import { ReturnType } from "../../type/ReturnType"
+import { error } from "console";
 
 export const PlayerSelect: React.FC = () => {
 
@@ -10,6 +14,23 @@ export const PlayerSelect: React.FC = () => {
 
     const [user, setUser] = useRecoilState(userAtom);
     
+        /* 대표 캐릭터 수정 */
+        const handleCharacterSelect = async (clickedCharacterId: number) => {
+            
+            setUser()
+
+            const payload: UpdateSelectedCharacter = {
+                previousId: user.characterId,
+                nowId: clickedCharacterId
+            };
+
+            await updateCharacter(payload => {
+
+            }, 
+            (error) => {
+                console.log(error);
+            });
+        };
 
     /* 소품 */
 
@@ -44,9 +65,16 @@ export const PlayerSelect: React.FC = () => {
         3: [0, -0.5, 0]
     }
 
+    const lockPositions: { [key: number]: [number, number, number] } = {
+        1: [-4, 1.5, 0],
+        2: [-2, 1.5, 0],
+        3: [0, 1.5, 0]
+    }
+
     // 글자 색 
     const getColorForName = (id: number) => user.characterId === id ? "red" : "black";
     
+
                           
     return (
         <>
@@ -59,11 +87,24 @@ export const PlayerSelect: React.FC = () => {
             object={aurora.scene}
         />
 
-        <primitive scale={0.1} position={[0, 1.5, 0]} object={lock.scene}/>
+        {/* 조건부 자물쇠 렌더링 */}
+            {user.lockList.map((lockInfo, index) => {
+                const positionIndex = index + 1; // 가정: positionIndex는 lockPositions와 맞춤
 
-        <primitive scale={1} position={characterPositions[1]} object={ManA.scene} />
-        <primitive scale={1} position={characterPositions[2]} object={WomanA.scene} />
-        <primitive scale={1} position={characterPositions[3]} object={WomanB.scene} />
+                // 만약 user.lockList[index].isLocked 가 true 라면 자물쇠를 렌더링합니다.
+                return lockInfo.islocked && (
+                    <primitive
+                        key={index} // 고유한 key를 사용해야 합니다.
+                        scale={0.1}
+                        position={lockPositions[positionIndex]} 
+                        object={lock.scene}
+                    />
+                );
+            })}
+
+        <primitive scale={1} position={characterPositions[1]} object={ManA.scene} onClick={() => handleCharacterSelect(1)} />
+        <primitive scale={1} position={characterPositions[2]} object={WomanA.scene} onClick={() => handleCharacterSelect(2)} />
+        <primitive scale={1} position={characterPositions[3]} object={WomanB.scene} onClick={() => handleCharacterSelect(3)} />
 
         {/* 캐릭터 이름 텍스트 */}
         {Object.entries(characterPositions).map(([key, position]) => {
