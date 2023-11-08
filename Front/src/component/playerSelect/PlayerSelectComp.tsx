e2d951d5675c797109a90798c28df1cc729196ab
신산하 <tksgk2598@naver.com>
2023-11-09 02:22:20 +0900

    Refactor/FE: PlayerSelect 코드 정리

--- Front/src/component/playerSelect/PlayerSelectComp.tsx
+++ Front/src/component/playerSelect/PlayerSelectComp.tsx
@@ -1,26 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF, Environment, Text, useAnimations } from "@react-three/drei";
-import * as THREE from 'three';
 import { userAtom } from "../../atom/UserAtom";
 import { PlayerSelectAtom } from "../../atom/PlayerSelectAtom";
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { updateCharacter } from "../../api/User"
-import { UpdateSelectedCharacter } from "../../type/UserType";
-import { CharacterResponseType } from "../../type/CharacterType";
-// import { SelectButtonComp } from "./SelectButtonComp";
-import { KeyPressed, AnimationAction, NpcInfo, CurrentNpc } from "../theme/ThemeType"
+import { AnimationAction } from "../theme/ThemeType"
 import { SetAction } from '../theme/util/PlayerMoveUtil';
 
 export const PlayerSelect: React.FC = () => {
 
   /* User Info */
-
   const [user, setUser] = useRecoilState(userAtom);
   const selectPlayer = useRecoilValue(PlayerSelectAtom);
-
-  /* 소품 */
-  const lock = useGLTF(import.meta.env.VITE_S3_URL + "Objects/Lock1/scene.gltf")
-
 
   /* Characters */
   
@@ -38,33 +28,6 @@ export const PlayerSelect: React.FC = () => {
   const F6 = useGLTF(import.meta.env.VITE_S3_URL + "NPC/f_22.glb");
   const userFileList = [M1, F1, M2, F2, M3, F3, M4, F4, M5, F5, M6, F6];
 
-  const characterPositions: { [key: number]: [number, number, number] } = {
-      1: [-4, -0.5, 0],
-      2: [-2, -0.5, 0],
-      3: [0, -0.5, 0],
-      4: [2, -0.5, 0], 
-      5: [4, -0.5, 0]
-  };
-
-  const textOffsetY = -0.8;
-  
-
-  const lockPositions: { [key: number]: [number, number, number] } = {
-      1: [-4, 1.5, 0],
-      2: [-2, 1.5, 0],
-      3: [0, 1.5, 0],
-      4: [2, 1.5, 0],
-      5: [4, 1.5, 0]
-  }
-
-  const buttonPositions: { [key: number]: [number, number, number] } = {
-      1: [-2.8, -1.2, 3],
-      2: [-1.4, -1.2, 3],
-      3: [0, -1.2, 3], 
-      4: [1.4, -1.2, 3],
-      5: [2.8, -1.2, 3]
-  }
-
   // character motion
   const player = userFileList[selectPlayer];
   const playerAction = useRef<AnimationAction>();
@@ -75,73 +38,24 @@ export const PlayerSelect: React.FC = () => {
     setTimeout(()=>{SetAction('Idle', playerAction, playerActions, null);}, 700)
   },[selectPlayer])
 
-  // 글자 색 
-  const getColorForName = (id: number) => user.characterId === id ? "red" : "black";
   
   
   return (
-      <>
+    <>
       <Environment preset="sunset" />
 
 
-      {/* 조건부 자물쇠 렌더링 */}
-      {/* {user.lockList.map((lockInfo, index) => {
-          const characterId = index + 1; // 캐릭터 ID는 인덱스에 1을 더한 값입니다.
-
-          // user.lockList에서 islocked가 true인 경우에 해당 캐릭터 위치에 자물쇠를 표시합니다.
-          if (lockInfo.islocked) {
-
-              return (
-                  <primitive
-                      key={`lock-${characterId}`} // 각 자물쇠에 고유한 key를 부여합니다.
-                      scale={0.1}
-                      position={lockPositions[characterId]} // lockPositions에서 characterId에 해당하는 위치를 가져옵니다.
-                      object={lock.scene.clone()} // .clone()을 호출하여 각 자물쇠에 대해 별도의 인스턴스를 만듭니다.
-                  />
-              );
-          }
-          return null; // islocked가 false이면 null을 반환하여 렌더링하지 않습니다.
-      })} */}
 
       {/* 선택된 캐릭터 */}
       <primitive scale={1} position={[0, -1, 0]} object={userFileList[selectPlayer].scene} />
-      
+    
 
-      {/* 캐릭터 이름 텍스트 및 선택 버튼 */}
-      {Object.entries(characterPositions).map(([key, position]) => {
-          const id = Number(key); 
-          const buttonPosition = buttonPositions[id];
+      <Text
+          position={[0,0.5,-3]}
+      >
+        Choose your Player
+      </Text>
 
-          return (
-              <React.Fragment key={id}>
-
-                  {/* <Text
-                      key={`button-${key}`}
-                      position={[position[0], position[1] + textOffsetY - 0.5, position[2]]}
-                      fontSize={0.3}
-                      color={getColorForName(id)}
-                      onClick={() => handleCharacterSelect(id)}
-                  >
-                      Select
-                  </Text> */}
-
-                  {/* <SelectButtonComp
-                      key={`select-button-${id}`} // 고유한 key 값을 추가합니다.
-                      x={buttonPosition[0]}
-                      y={buttonPosition[1]}
-                      z={buttonPosition[2]}
-                  /> */}
-
-              </React.Fragment>
-          );
-      })}
-
-          <Text
-              position={[0,0.5,-3]}
-          >
-            Choose your Player
-          </Text>
-
-      </>
+    </>
   );
 };
