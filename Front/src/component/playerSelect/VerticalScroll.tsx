2a365b4e6e6e2f95388f34e706be35d3f6feb9af
신산하 <tksgk2598@naver.com>
2023-11-08 22:51:27 +0900

    Fix/FE: 스크롤바 위쪽만 길게 빠진 이유 해결

--- Front/src/component/playerSelect/VerticalScroll.tsx
+++ Front/src/component/playerSelect/VerticalScroll.tsx
@@ -8,21 +8,22 @@ const VerticalScroll = () => {
   const PlayerImgList = ["/selectPlayer/m1Img.png", "/selectPlayer/f11Img.png", "/selectPlayer/m31Img.png", "/selectPlayer/f20Img.png", "/selectPlayer/m11Img.png", "/selectPlayer/f12Img.png", "/selectPlayer/m14Img.png", "/selectPlayer/f14Img.png", "/selectPlayer/m29Img.png", "/selectPlayer/f21Img.png", "/selectPlayer/m28Img.png", "/selectPlayer/f22Img.png"];
 
   return (
-    // Add a max height and overflow-y-auto to allow vertical scrolling
-    <div
-      style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_small.png'), auto` }}  
-      className="absolute z-30 left-6 w-[13%] h-[100%] overflow-y-auto flex items-center select-none">
-      <div className="flex flex-col overflow-y-auto h-[90%]">
-        {PlayerImgList.map((img, index)=>{
-          return(
-            <div key={index}
-              style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }} 
-              className={selPlayer === index ? "rounded-xl mb-5 shadow-md bg-[#BDA4D5] h-[160px]" : "rounded-xl mb-5 shadow-md bg-white h-[160px]"}
-              onClick={() => setSelPlayer(index)}> {/**1부터 시작하는 id */}
-              <img className="rounded-xl w-[100%] h-[100%]" src={img} />
-            </div>
-          )
-        })}       
+    <div className="absolute z-30 w-[16%] h-[100%] flex items-center justify-center ml-3">
+      <div
+        style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_small.png'), auto` }}  
+        className="w-[100%] h-[90%] rounded-xl overflow-y-auto flex justify-center select-none">
+        <div className="flex flex-col max-w-[300px] h-[90%]">
+          {PlayerImgList.map((img, index)=>{
+            return(
+              <div key={index}
+                style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }} 
+                className={selPlayer === index ? "rounded-xl mb-6 shadow-md bg-[#BDA4D5] h-[160px]" : "rounded-xl mb-6 shadow-md bg-white h-[160px]"}
+                onClick={() => setSelPlayer(index)}> {/**1부터 시작하는 id */}
+                <img className="rounded-xl w-[100%] h-[100%]" src={img} />
+              </div>
+            )
+          })}       
+        </div>
       </div>
     </div>
   );
