5b755db2e6b84d4163f929dfa5c4bdef13813065
신산하 <tksgk2598@naver.com>
2023-11-09 02:34:55 +0900

    Feat/FE: 자물쇠 움직이는 효과 추가

--- Front/src/component/playerSelect/VerticalScroll.tsx
+++ Front/src/component/playerSelect/VerticalScroll.tsx
@@ -1,24 +1,25 @@
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
+import * as THREE from 'three';
 import { userAtom } from '../../atom/UserAtom';
 import { PlayerSelectAtom } from "../../atom/PlayerSelectAtom";
-import { useRecoilState } from 'recoil';
-import { Canvas } from '@react-three/fiber';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { useGLTF, Environment } from "@react-three/drei";
 import { showToaster } from "../../pages/PlayerSelectPage";
 
 const VerticalScroll = () => {
   
   /* User Info */
-  const [user, setUser] = useRecoilState(userAtom);
+  const user = useRecoilValue(userAtom);
   const [selPlayer, setSelPlayer] = useRecoilState(PlayerSelectAtom); //이전 캐릭터 선택 기록이 있다면 캐릭터 index번호 : 아니면 -1
   const PlayerImgList = ["/selectPlayer/m1Img.png", "/selectPlayer/f11Img.png", "/selectPlayer/m31Img.png", "/selectPlayer/f20Img.png", "/selectPlayer/m11Img.png", "/selectPlayer/f12Img.png", "/selectPlayer/m14Img.png", "/selectPlayer/f14Img.png", "/selectPlayer/m29Img.png", "/selectPlayer/f21Img.png", "/selectPlayer/m28Img.png", "/selectPlayer/f22Img.png"];
 
-  /* 3D 자물쇠 */
-  const lock = useGLTF(import.meta.env.VITE_S3_URL + "Objects/Lock1/scene.gltf");
-
+  /* 미획득 플레이어 체크 후 설정 */
   const settingPlayer = (index:number) => {
     if(!user.lockList[index].islocked) setSelPlayer(index);
   }
+
+  
 
   return (
     <div className="absolute z-30 w-[16%] h-[100%] flex items-center justify-center ml-3">
@@ -42,7 +43,10 @@ const VerticalScroll = () => {
                     <Canvas>
                       <Suspense fallback={null}>
                         <Environment preset="sunset" />
-                        <primitive object={lock.scene.clone()} scale={0.3} position={[3, -3, 0]} key={`lock-${index}`}/>
+                        <Lock
+                          isLocked={user.lockList[index].islocked}
+                          position={[3, -3, 0]}
+                        />
                       </Suspense>
                     </Canvas>
                   </div>
@@ -56,6 +60,22 @@ const VerticalScroll = () => {
       </div>
     </div>
   );
+};
+
+/* 3D 자물쇠 */
+const Lock = ({ isLocked, position, onClick }: any) => {
+  const lock = useGLTF(import.meta.env.VITE_S3_URL + "Objects/Lock1/scene.gltf");
+  const lockRef = useRef<THREE.Mesh>(null);
+  useFrame(() => {
+    const time = Date.now() * 0.005;
+    if (lockRef.current) lockRef.current.rotation.y = Math.sin(time) * 0.2;
+  });
+
+  return isLocked ? (
+    <mesh onClick={onClick} position={position} ref={lockRef}>
+      <primitive object={lock.scene.clone()} scale={0.3} />
+    </mesh>
+  ) : null;
 };
 
 export default VerticalScroll;
