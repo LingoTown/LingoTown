af86068d06178111b27d3246aa13b26792e86983
ParkSeYun98 <yun12343@naver.com>
2023-11-09 11:29:41 +0900

    Feat/BE: Intimacy 출력 API

---
+++ Back/src/main/java/com/lingotown/domain/talk/dto/response/IntimacyResDto.java
@@ -0,0 +1,13 @@
+package com.lingotown.domain.talk.dto.response;
+
+import lombok.*;
+
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class IntimacyResDto {
+
+    private Long memberNpcId;
+    private Long memberId;
+    private Long npcId;
+    private int intimacy;
+}
--- Back/src/main/java/com/lingotown/domain/talk/dto/response/ReadMemberNPCResDto.java
+++ Back/src/main/java/com/lingotown/domain/talk/dto/response/ReadMemberNPCResDto.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class ReadMemberNPCResDto {
+
     private Long memberNPCId;
     private int talkCount;
     private int intimacy;
