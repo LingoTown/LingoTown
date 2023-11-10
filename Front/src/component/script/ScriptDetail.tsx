fd399264da79efd94dcb855d276f26d1f6bc0bbd
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-11-10 17:43:04 +0900

    Feat/FE: 문법체크 로직

--- Front/src/component/script/ScriptDetail.tsx
+++ Front/src/component/script/ScriptDetail.tsx
@@ -5,47 +5,86 @@ import { HttpJson } from '../../api/common/Http';
 import { talkDetailType } from '../../type/TalkType';
 import { detailVerAtom } from '../../atom/ScriptAtom';
 import { npcStateName } from '../../atom/ScriptAtom';
+import { grammarCheckType } from '../../type/TalkListType';
 function ScriptDetail() {
     const [talkId, ] = useRecoilState(talkIdAtom);
     const [detailList, setDetailList] = useState<talkDetailType[]>([]);
     const [showCorr, setShowCorr] = useState<boolean[]>([]);
     const [, setDetailVerAtom] = useRecoilState(detailVerAtom);
+    const [savedata, setData] = useState<grammarCheckType[]>([])
     const [npcName, ] = useRecoilState(npcStateName);
-    const [corrMode, setCorrMode] = useState(false);
     const audioRefs = useRef<Array<HTMLAudioElement | null>>([]);
+
     useEffect(() => {
       console.log(npcName);
       HttpJson.get(`/api/talk/${talkId}`)
           .then((res) => {
-            console.log(res.data.data);
             const reversedData = [...res.data.data];
             const arr = new Array(reversedData.length).fill(false);
+            //dataList의 길이만큼 문법체크가 이루어지니까, 그 길이만큼 배열을 만든다.
+            setData(new Array(reversedData.length).fill({}));
             setShowCorr(arr); 
               setDetailList(reversedData);
-              // Reset the refs array to match the number of detailList items
               audioRefs.current = reversedData.map(() => null);
           })
           .catch(console.log);
-  }, [talkId]);
+  }, []);
 
   //showCorr 배열 조정
   const handleCorrMode = (i:number) => {
     const arr = [...showCorr];
     arr[i] = !arr[i];
     setShowCorr(arr);
-    
   }
 
-  const handleGrammar = (talkDetailId:number, content:string) => {
-    HttpJson.get(`api/talk/score/${talkDetailId}`)
+  useEffect(() => {
+    detailList.forEach((arr, i) => {
+      if (showCorr[i]) {
+        handleGrammar(arr.talkDetailId, i);
+      }
+    });
+  }, [showCorr, detailList]);
+
+  const handleGrammar = (talkDetailId:number, index:number) => {
+      HttpJson.get(`/api/talk/score/${talkDetailId}`)
       .then((res)=>{
-        console.log(res.data.data);
+        // console.log(res.data.data);
+        const temp = [...savedata];
+        temp[index] = res.data.data;
+        setData(temp);
       })
       .catch(console.log)
       return(
         <>
-        <div className='text-white'>test! {talkDetailId}</div>
-        <div className='text-white'>{content}</div>
+        <div className='flex flex-col font-[30] ml-5 text-blue-100 p-1 px-3 border-[1px] border-blue-100 rounded-[7px]' >
+          <div className='w-1/2 flex flex-row justify-between text-blue-200 mb-2'>
+            <div>총점 : {savedata[index].overallScore}점</div>|
+            <div>유창성 : {savedata[index].fluencyScore}점</div>|
+            <div>정확도 : {savedata[index].integrityScore}점</div>|
+            <div>발음 : {savedata[index].pronunciationScore}점</div>|
+            <div>강세 : {savedata[index].rhythmScore}점</div>
+          </div>
+
+          <div>[단어별 발음 평가] &nbsp;</div>
+          <div className='flex flex-row '>
+            {
+              savedata[index].wordScoreList?.map((item, i) => (
+                <div key={i} className=''>
+                  {
+                    item.score >= 70?
+                    <div>
+                      {item.word}&nbsp;
+                    </div>
+                    :
+                    <div className='text-red-300'>
+                      {item.word}&nbsp;
+                    </div>
+                  }
+                </div>
+              ))
+            }
+          </div>
+        </div>
         </>
       )
   }
@@ -64,13 +103,13 @@ function ScriptDetail() {
           <div className="flex flex-row justify-between">
             <div className="m-5 my-0 text-white font-['passero-one'] font-[30] text-[1.8rem] ">
               My Talk Script &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <span 
+              {/* <span 
                 
                 className='hover:text-[1.15rem] text-[1.2rem] text-red-300'
                 style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }}
               >View correction
               <span className='material-icons text-[1.1rem]'>&nbsp;visibility</span>
-              </span>
+              </span> */}
             </div>
             <div onClick={()=>{setDetailVerAtom(false)}} 
             className="hover:text-[1.2rem] align-center font-['passero-one'] font-[30] text-white text-[1.3rem] mr-5" 
@@ -94,22 +133,25 @@ function ScriptDetail() {
                         </span>
                         {/* 문장 하나씩 */}
                         <div 
-                          onClick={()=>{handleCorrMode(i)}}
-                          className='m-1 mt-2 text-white font-[30] hover:text-red-300'>
+                          onClick={()=>{
+                            handleCorrMode(i)
+                          }}
+                          className='m-1 mt-2 text-white font-[30] hover:text-blue-200'>
                           {
                             arr.member == false?
                             <span>{npcName} : </span>
                             :
                             <span>Me : </span>
                           }
-                          {arr.content}
+                          
+                            {arr.content}
+                          
                         </div>
                       </div>
                       {/* 문법 검사 내용 */}
                       {
-                        showCorr[i] && 
-                        handleGrammar(arr.talkDetailId, arr.content)
-                        // <div className='text-white -mt-1 ml-10'>test {arr.talkDetailId}</div>
+                        showCorr[i] &&
+                        handleGrammar(arr.talkDetailId, i)
                       }
                       </>
                     );
