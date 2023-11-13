06a783d7e7b60d45002bb3c6f9c3225e2ebe9ee8
신산하 <tksgk2598@naver.com>
2023-11-13 11:03:18 +0900

    Fix/FE: 페이지 사이즈 줄였을 때 3D 재렌더링 시도

--- Front/src/pages/ThemePage.tsx
+++ Front/src/pages/ThemePage.tsx
@@ -1,19 +1,45 @@
-import { Canvas } from "@react-three/fiber";
-import { useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { loadingAtom } from "../atom/LoadingAtom";
-import { ThemeComp } from "../component/category/ThemeComp";
-import LoadingPage from "./LoadingPage";
+import { useEffect, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { loadingAtom } from '../atom/LoadingAtom';
+import { ThemeComp } from '../component/category/ThemeComp';
+import LoadingPage from './LoadingPage';
 
 export const ThemePage = () => {
 
   const [loading, setLoading] = useRecoilState(loadingAtom);
   const navigate = useNavigate();
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    const canvasElement = canvasRef.current;
+
+    const handleContextLost = (event: Event) => {
+      event.preventDefault();
+      // handle context loss
+    };
+
+    const handleContextRestored = (event: Event) => {
+      // handle context restoration
+    };
+
+    canvasElement?.addEventListener('webglcontextlost', handleContextLost as EventListener);
+    canvasElement?.addEventListener('webglcontextrestored', handleContextRestored as EventListener);
+
+    return () => {
+      canvasElement?.removeEventListener('webglcontextlost', handleContextLost as EventListener);
+      canvasElement?.removeEventListener('webglcontextrestored', handleContextRestored as EventListener);
+    };
+  }, []);
+
 
   return(
     <div
       style={{
-        backgroundImage: `url(${import.meta.env.VITE_S3_URL}BackGround/cloud_background.png)`
+        backgroundImage: `url(${import.meta.env.VITE_S3_URL}BackGround/cloud_background.png)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
     >
       {
@@ -21,7 +47,7 @@ export const ThemePage = () => {
       }
       {
         !loading.loading ?
-        <div className="pt-8 px-5 -mb-12 flex items-center text-5xl font-bold text-[#5dc7f8] absolute w-full font-['GabiaSolmee']"
+        <div className="pt-8 px-5 flex place-content-between text-5xl font-bold text-[#5dc7f8] absolute w-full font-['GabiaSolmee']"
           style={{zIndex:"1"}}
         >
           <div className="flex-grow ml-4 drop-shadow-lg" style={{minWidth: '33%', cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }}>
@@ -44,7 +70,7 @@ export const ThemePage = () => {
         null
       }
 
-      <Canvas style={{zIndex:"0", height:loading.loading?"0.01vh":"100vh" }} camera={{ position: [0, 0, 10], fov: 30 }}>
+      <Canvas ref={canvasRef} style={{zIndex:"0", height:loading.loading?"0.01vh":"100vh" }} camera={{ position: [0, 0, 10], fov: 30 }}>
         <ThemeComp/>
       </Canvas>
 
