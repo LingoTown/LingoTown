c3fc7e1b45e422935fa0e37a6b8ef8f3e58098a9
신산하 <tksgk2598@naver.com>
2023-11-13 10:42:43 +0900

    Style/FE: 포탈 위치 조정

--- Front/src/component/category/MapEnterComp.tsx
+++ Front/src/component/category/MapEnterComp.tsx
@@ -54,13 +54,13 @@ export const MapEnterComp: React.FC<{
       }}
     >
       <RoundedBox
-        args={[0.8, 0.25, 0.1]}
+        args={[0.7, 0.25, 0.1]}
       >
         <meshStandardMaterial attach="material" color={((language === 0 || language === 2) && name !== "아트 갤러리") || (language === 1 && name === "아트 갤러리") ? "#5dc7f8" : "#d67777"} />
         {
           ((language === 0 || language === 2) && name !== "아트 갤러리") || (language === 1 && name === "아트 갤러리") ?
-            <TextUtil x={0} y={0} z={0.051} color="black" size={0.15} name={text[0]} /> :
-            <TextUtil x={0} y={0} z={0.051} color="black" size={0.15} name={text[1]} />
+            <TextUtil x={0} y={0} z={0.051} color="black" size={0.13} name={text[0]} /> :
+            <TextUtil x={0} y={0} z={0.051} color="black" size={0.13} name={text[1]} />
         }
       </RoundedBox>
     </mesh>
