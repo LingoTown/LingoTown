51fbc1aae8abf4e48645b04beee8df319364d9cd
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-11-13 10:25:17 +0900

    Feat/BE: 하나의 대화에서 전체 정보 조회하기 생성

---
+++ Back/src/main/java/com/lingotown/domain/talk/dto/response/ReadTalkDetailInfoResDto.java
@@ -0,0 +1,19 @@
+package com.lingotown.domain.talk.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReadTalkDetailInfoResDto {
+    private LocalDateTime createAt;
+    private ReadTalkDetailResDto talkDetailList;
+    private ReadPronunciationScoreResDto pronunciationScoreList;
+}
