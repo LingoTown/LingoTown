93fd1f339c484e505056f69fcba376435f09b9de
지민성 <jiminsung@naver.com>
2023-11-14 12:46:13 +0900

    Feat/FE: 회원탈퇴후 메인페이지 이동

--- Front/src/pages/LoginPage.tsx
+++ Front/src/pages/LoginPage.tsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
   const setLoading = useSetRecoilState(loadingAtom);
 
   useEffect(() => {
-    if (user.accessToken !== "") {
+    if (user.email !== "") {
       navigate("/departure");
     }
   }, []);
