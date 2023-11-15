4eb6141577997a8e1089236b758fab0063e9e2b4
신산하 <tksgk2598@naver.com>
2023-11-15 22:41:05 +0900

    Remove/FE: LOADING 기존 방식 추구

--- Front/src/pages/LoadingPage.tsx
+++ Front/src/pages/LoadingPage.tsx
@@ -1,25 +1,25 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Lottie from 'lottie-react';
-import loading from '../../public/loading_bar.json';
+
 
 const LoadingPage = () => {
 
 
-  // const [animationData, setAnimationData] = useState(null);
+  const [animationData, setAnimationData] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchAnimationData = async () => {
-  //     try {
-  //       const response = await fetch(`${import.meta.env.VITE_S3_URL}Effect/Loading+Bar/progress.json`);
-  //       const data = await response.json();
-  //       setAnimationData(data);
-  //     } catch (error) {
-  //       console.error('Error fetching animation data:', error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchAnimationData = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_S3_URL}Effect/Loading+Bar/progress.json`);
+        const data = await response.json();
+        setAnimationData(data);
+      } catch (error) {
+        console.error('Error fetching animation data:', error);
+      }
+    };
 
-  //   fetchAnimationData();
-  // }, []);
+    fetchAnimationData();
+  }, []);
 
   const cursorLink = import.meta.env.VITE_S3_URL + 'MousePointer/navigation_small.png'; //마우스 커서 기본
   return(
@@ -32,15 +32,15 @@ const LoadingPage = () => {
       className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center z-30" 
       style={{ backgroundImage: `url('${import.meta.env.VITE_S3_URL}BackGround/WorldBackground.png')`, cursor: `url('${cursorLink}'), auto` }}
 >
-      <div className="text-center z-30 mt-[100px]">
+      <div className="text-center z-30">
         <div>
           <h1 className="text-5xl font-bold text-[#fff] font-['passero-one'] pb-5">Explore New Language with</h1>
           <h2 className="text-8xl mt-2 font-extrabold text-[#FFE58A] font-['passero-one']">LingoTown</h2>
           <img className='ml-[520px] mt-[-120px]' src={`${import.meta.env.VITE_S3_URL}Effect/Loading+Bar/shineEffect.png`}/>
         </div>
       </div>
-      <div className="z-30">
-        <Lottie animationData={loading} />
+      <div className="z-30 mt-[100px]">
+        <Lottie animationData={animationData} />
       </div>
     </div>
   </>
