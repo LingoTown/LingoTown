3642f481aabfb93619e0e98f8565e5692c775cbb
신산하 <tksgk2598@naver.com>
2023-11-15 21:21:44 +0900

    Feat/FE: next/prev btn setting

--- Front/src/component/tutorial/Tutorial.tsx
+++ Front/src/component/tutorial/Tutorial.tsx
@@ -8,7 +8,7 @@ import Lottie from 'lottie-react';
 const cursorLink = import.meta.env.VITE_S3_URL + 'MousePointer/navigation_small.png'; // 기본 
 const cursorHoverLink = import.meta.env.VITE_S3_URL + 'MousePointer/navigation_hover_small.png'; //hover
 
-const Tutorial = () => {
+const Tutorial: React.FC = () => {
   const [step, setStep] = useState<number>(0);
   const [isPressed, setIsPressed] = React.useState(false);
   const img:string[] = ["", "Tutorial/keyboard_handling.png", `Tutorial/DirectionalKey.png`, `Tutorial/SpaceBarkey.png`, `Tutorial/A_key.png`, `Tutorial/S_key.png`, `Tutorial/SS_key.png`, `Tutorial/D_key.png`, `Tutorial/quest.png`, `Tutorial/quest_character.png`, `Tutorial/freeTalking.png`, `Tutorial/free_start.png`, `Tutorial/free_send.png`, `Tutorial/free_retry.png`, `Objects/free_history.png`, `Tutorial/free_end.png`, `Tutorial/topic.png`, `Tutorial/topic_select.png`, `Tutorial/topic_listen.png`, `Tutorial/topic_translate.png`, `Objects/topic_voca.png`, `Tutorial/topic_answer.png`, `Tutorial/topic_end.png`, `Tutorial/record.png`, `Tutorial/topic_record.png`, `Tutorial/guide.png`];
@@ -16,6 +16,16 @@ const Tutorial = () => {
   const statement:string[] = ["", "", "방향키로 링고타운을 돌아다녀 보세요!", "스페이스바를 눌러 신나게 점프해볼까요?!", "링고타운 주민들에게 가까이 다가가 A키로 말을 걸어보세요!", "빔프로젝트가 있다면? 가까이 다가가 S키를 눌러 영상을 감상해보세요!", "S키를 2번 누르면 일시정지가 됩니다!", "어디서든 D키를 눌러 영상을 종료하실 수 있습니다!","", "퀘스트를 해결하며 NPC와 친밀도도 올리고, 새로운 캐릭터도 획득해보세요!", "", "Start Talk 버튼을 누르면 프리토킹 시작입니다!", "NPC에게 하고 싶은 말을 하고 Send 버튼을 눌러주세요!", "다시 말씀하시고 싶으시다면 Retry 버튼을 눌러주세요!", "History 버튼을 눌러 대화 내역을 확인하세요!", "End를 누르시면 대화가 종료됩니다!", "", "NPC별로 준비된 Topic을 선택해 대화하실 수 있습니다.", "Listen Again버튼을 눌러 NPC의 질문을 다시 들어보세요!", "NPC 말이 해석되지 않는다면? Translate 버튼을 눌러보세요!", "단어가 생각이 나지 않는다면? Help 버튼을 눌러보세요!", "말할 답변을 생각하고, start talk 버튼을 눌러 대답해보세요!", "End버튼을 눌러 NPC와의 대화를 종료하실 수 있습니다!", "", "모든 대화 기록은 Mypage에서 확인하고 재학습 하실 수 있습니다!", "궁금한 점이 있으시다면 언제든 가이드 버튼을 눌러 확인해주세요!"]; 
   const setVisit = useSetRecoilState(tutorialAtom);
   const [animationData, setAnimationData] = useState(null);
+
+  // 이미지 상태 관리
+  const [imageLeftSrc, setImageLeftSrc] = useState('/left_btn.png');
+  const [imageRightSrc, setImageRightSrc] = useState('/right_btn.png');
+
+  // 마우스 호버 이벤트 핸들러
+  const onMouseOverLeft = () => setImageLeftSrc('/left_btn_hover.png');
+  const onMouseOutLeft = () => setImageLeftSrc('/left_btn.png');
+  const onMouseOverRight = () => setImageRightSrc('/right_btn_hover.png');
+  const onMouseOutRight = () => setImageRightSrc('/right_btn.png');
 
 
   // 마우스가 버튼 위로 올라왔을 때와 떠났을 때의 이벤트 핸들러
@@ -46,6 +56,13 @@ const Tutorial = () => {
     setStep(step+1);
   }
 
+  const prevStep = () => {
+    if(step == 0) {
+      return;
+    }
+    setStep(step-1);
+  }
+
   const moveStep = (index: number) => {
     setStep(index);
   }
@@ -69,25 +86,58 @@ const Tutorial = () => {
       }}
       className="absolute z-50 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#fff]/80 max-w-[700px] w-full md:w-[700px] h-auto rounded-[20px] p-8"
     >
-      <div 
+      {/* <div 
         onClick={skipStep}
         style={{ cursor: `url(${cursorHoverLink}), auto` }} 
         className="font-[NPSfontBold] text-[#09B1F8] font-['ARLRDBD'] text-[13px] font-bold ml-[95%]">
         SKIP
+      </div> */}
+
+      {/* 왼 / 이미지 / 오 */}
+      <div  className='flex w-[100%] h-[100%] max-h-[300px]'>
+        {/* 왼 */}
+        <div
+            style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }}
+            className={step==0?'invisible':'flex justify-center items-center'}>
+            <img
+                className='w-[70%]'
+                src={imageLeftSrc}
+                onMouseOver={onMouseOverLeft}
+                onMouseOut={onMouseOutLeft}
+                onClick={prevStep}
+                alt="Button"
+            />
+        </div>
+
+        {/* 이미지 */}
+        <div className='w-[100%] h-[100%] flex justify-center items-center'>
+          {
+            step===0?
+            <Lottie className="mx-auto max-w-[270px]" animationData={animationData} />
+            :
+            <img className={ step==8 || step==16 || step==23? "mx-auto max-w-[200px]" : ( step==10 || step==14 || step==25? "mx-auto max-w-[250px]":(step==24?"mx-auto max-w-[300px]":(step==9 ? "mx-auto max-w-[350px]" : "mx-auto max-w-[400px]")))} src={`${import.meta.env.VITE_S3_URL}`+img[step]} />
+          }
+        </div>
+
+        {/* 오 */}
+        <div
+            style={{ cursor: `url('${import.meta.env.VITE_S3_URL}MousePointer/navigation_hover_small.png'), auto` }}
+            className={step==title.length-1?'invisible':'flex justify-center items-center'}>
+            <img
+                className='w-[70%]'
+                src={imageRightSrc}
+                onMouseOver={onMouseOverRight}
+                onMouseOut={onMouseOutRight}
+                onClick={nextStep}
+                alt="Button"
+            />
+        </div>
+
       </div>
 
-      {/* 이미지 */}
-      <div className='w-[100%] h-[100%] max-h-[500px] flex justify-center items-center'>
-        {
-          step===0?
-          <Lottie className="mx-auto max-w-[270px]" animationData={animationData} />
-          :
-          <img className={ step==8 || step==16 || step==23? "mx-auto max-w-[200px]" : ( step==10 || step==14 || step==25? "mx-auto max-w-[250px]":(step==24?"mx-auto max-w-[300px]":(step==9 ? "mx-auto max-w-[350px]" : "mx-auto max-w-[400px]")))} src={`${import.meta.env.VITE_S3_URL}`+img[step]} />
-        }
-      </div>
 
       {/* 타이틀 */}
-      <div className="font-[NPSfontBold] text-center text-[20px] font-bold mt-7">
+      <div className="font-[NPSfontBold] text-center text-[20px] font-bold mt-10">
         {title[step]}
       </div>
 
@@ -106,22 +156,8 @@ const Tutorial = () => {
         }
       </div>
 
-      <div 
-        style={{ cursor: `url(${cursorLink}), auto` }} 
-        className="font-[NPSfontBold] flex justify-center items-center mt-6 space-x-2">
-        <button
-          style={applyPressedStyle(isPressed)}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseLeave}
-          onClick={nextStep}
-        >
-          {title.length-1==step?"End":"Next"}
-        </button>
-      </div>
-
       {/* next page */}
-      <div className="flex justify-center items-center mt-8 space-x-2">
+      {/* <div className="flex justify-center items-center mt-8 space-x-2">
         {
           title.map((_, index)=>{
             return(
@@ -129,7 +165,7 @@ const Tutorial = () => {
             )
           })
         }
-      </div>
+      </div> */}
     </div>
   );
 }
