878ea836281f3ccb187ba027446ffd959cf3fcd8
ParkSeYun98 <yun12343@naver.com>
2023-11-15 21:36:59 +0900

    Feat/BE: 대화 동기화

--- Back/src/main/java/com/lingotown/domain/talk/service/OpenAIService.java
+++ Back/src/main/java/com/lingotown/domain/talk/service/OpenAIService.java
@@ -196,6 +196,117 @@ public class OpenAIService {
                 ResponseStatus.CREATED_SUCCESS.getMessage(), openAIResDto);
     }
 
+    @Transactional
+    public DataResponse<CreateOpenAIResDto> askGPTSync(Principal principal, TalkReqDto talkReqDto) throws Exception {
+        Gson gson = new Gson();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(APIKEY);
+
+        //요청을 담을 메세지 리스트
+        List<OpenAIMessageDto> messages = new ArrayList<>();
+
+        //이전 대화를 담을 리스트
+        List<OpenAIMessageDto> chatList = new ArrayList<>();
+
+
+        //이전 대화가 없을 경우
+        if (!cacheService.hasCache(talkReqDto.getTalkId())) {
+            String concept = "";
+
+            if (talkReqDto.getTalkFile() != null) concept = createConcept(principal, talkReqDto.getTalkId(), null);
+            else concept = createConcept(principal, talkReqDto.getTalkId(), talkReqDto.getPrompt());
+
+            // AI 역할부여
+            OpenAIMessageDto messageDtoAI = OpenAIMessageDto
+                    .builder()
+                    .role("assistant")
+                    .content(concept)
+                    .build();
+
+            //메세지 리스트에 담기
+            messages.add(messageDtoAI);
+
+            //이전 대화가 있을 경우 내용을 가져와서 추가
+        } else {
+            List<OpenAIMessageDto> previousChatDataList
+                    = cacheService.getAllPreviousChatData(talkReqDto.getTalkId());
+            messages.addAll(previousChatDataList);
+        }
+
+        // user 인풋
+        if (talkReqDto.getTalkFile() != null) {
+            OpenAIMessageDto messageDtoUser = OpenAIMessageDto
+                    .builder()
+                    .role("user")
+                    .content(talkReqDto.getPrompt())
+                    .build();
+            messages.add(messageDtoUser);
+        }
+
+        //요청Dto
+        OpenAIReqDto requestDto = OpenAIReqDto
+                .builder()
+                .max_tokens(50)
+                .messages(messages)
+                .build();
+        String jsonString = gson.toJson(requestDto);
+
+        String body = jsonString;
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        //HTTP 요청
+        ResponseEntity<OpenAIResDto> response = restTemplate.exchange(GPTURL, HttpMethod.POST, entity, OpenAIResDto.class);
+
+        //현재 요청과 응답 캐싱
+        OpenAIMessageDto responseDto = OpenAIMessageDto
+                .builder()
+                .role("assistant")
+                .content(response.getBody().getChoices()[0].getMessage().getContent())
+                .build();
+
+        chatList.addAll(messages);
+        chatList.add(responseDto);
+        cacheService.cacheTalkData(talkReqDto.getTalkId(), chatList);
+
+        MultipartFile GPTResponseFile = ttsService.UseTTS(responseDto.getContent(), talkReqDto);
+        TalkDetail systemTalkDetail = createSystemTalkDetail(talkReqDto, GPTResponseFile , responseDto.getContent());
+
+        TalkDetail savedUserTalkDetail = null;
+        if (talkReqDto.getTalkFile() != null) {
+            Talk talk = getTalkEntity(talkReqDto.getTalkId());
+            String fileUrl = s3Service.uploadFile(talkReqDto.getTalkFile());
+
+            TalkDetail talkDetail = TalkDetail
+                    .builder()
+                    .isMember(true)
+                    .content(talkReqDto.getPrompt())
+                    .talkFile(fileUrl)
+                    .talk(talk)
+                    .grammarAdvise(null)
+                    .build();
+
+            savedUserTalkDetail = talkDetailRepository.save(talkDetail);
+        }
+
+        // 비동기적으로 발음 체크 실행
+        if (savedUserTalkDetail != null) {
+            performAsyncPronunciationCheck(savedUserTalkDetail, talkReqDto);
+        }
+
+        // System 응답 DB 저장
+
+
+
+        CreateOpenAIResDto openAIResDto = createOpenAIResponseDto(systemTalkDetail);
+
+        return new DataResponse<>(ResponseStatus.CREATED_SUCCESS.getCode(),
+                ResponseStatus.CREATED_SUCCESS.getMessage(), openAIResDto);
+    }
+
 
     private void performAsyncPronunciationCheck(TalkDetail talkDetail, TalkReqDto talkReqDto) throws IOException {
         webClientUtil.checkPronunciationAsync(SPEECHURL, APPKEY, SECRETKEY, talkReqDto)
@@ -213,6 +324,26 @@ public class OpenAIService {
                         return null;
                     });
                 });
+    }
+
+    private void performSyncPronunciationCheck(TalkDetail talkDetail, TalkReqDto talkReqDto) throws IOException {
+        // 동기 메서드 호출
+        String pronunciationResDtoAsString = webClientUtil.checkGrammarSync(SPEECHURL, APPKEY, SECRETKEY, talkReqDto);
+
+        // 응답 처리
+        PronunciationResDto pronunciationResDto;
+        try {
+            pronunciationResDto = new ObjectMapper().readValue(pronunciationResDtoAsString, PronunciationResDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 결과 업데이트
+        TransactionTemplate template = new TransactionTemplate(transactionManager);
+        template.execute(status -> {
+            updatePronunciationResults(talkDetail, pronunciationResDto);
+            return null;
+        });
     }
 
     // 발음 평가 DB 저장
